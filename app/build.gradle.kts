2146047330aaecb6e493b51fc82e04f1bae4ccc9
Brandon Cano <brandoncano01@gmail.com>
2025-03-16 01:29:01 -0500

    updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.brandoncano.capacitorcalculator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11 // for 2.2.0
-        versionName = "2.2.0"
+        versionCode = 12 // for 2.3.0
+        versionName = "2.3.0-develop"
 
         vectorDrawables {
             useSupportLibrary = true
@@ -23,7 +23,7 @@ android {
     applicationVariants.configureEach {
         val suffix = if (buildType.name == "debug") ", DEBUG" else ""
         resValue("string", "version", "$versionName$suffix")
-        resValue("string", "last_updated", "1/15/2025")
+        resValue("string", "last_updated", "3/16/2025")
     }
     buildTypes {
         release {
